64d3385a04669e9d2c6f0b8cf8ac534e4c16c176
windsnow1025 <windsnow1025@gmail.com>
2023-01-17 05:55:14 -0800

    Login

--- app/src/main/java/com/project/Fragment3.java
+++ app/src/main/java/com/project/Fragment3.java
@@ -1,15 +1,26 @@
 package com.project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class Fragment3 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment3, container, false);
+        View view = inflater.inflate(R.layout.fragment3, container, false);
+        TextView textView = view.findViewById(R.id.textViewLoginSignup);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), login.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
