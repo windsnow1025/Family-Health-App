40642d671c836143119577441cb51625bad8b676
windsnow1025 <windsnow1025@gmail.com>
2023-02-24 21:04:18 -0800

    Table Adapter

--- app/src/main/java/com/project/FragmentRecord.java
+++ app/src/main/java/com/project/FragmentRecord.java
@@ -6,20 +6,35 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FragmentRecord extends Fragment {
 
-        public FragmentRecord() {
-        }
+    View view;
+    public FragmentRecord() {
+    }
 
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-        }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View view = inflater.inflate(R.layout.fragment_record, container, false);
-            return view;
-        }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_record, container, false);
+
+        List<String[]> data = new ArrayList<>();
+        data.add(new String[]{"Row 1, Column 1", "Row 1, Column 2"});
+        data.add(new String[]{"Row 2, Column 1", "Row 2, Column 2"});
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(new TableAdapter(data));
+
+        return view;
+    }
 }
---
+++ app/src/main/java/com/project/TableAdapter.java
@@ -0,0 +1,50 @@
+package com.project;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class TableAdapter extends RecyclerView.Adapter<TableAdapter.ViewHolder> {
+
+    private List<String[]> data;
+
+    public TableAdapter(List<String[]> data) {
+        this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.table_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String[] rowData = data.get(position);
+        holder.column1.setText(rowData[0]);
+        holder.column2.setText(rowData[1]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView column1;
+        TextView column2;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            column1 = itemView.findViewById(R.id.column1);
+            column2 = itemView.findViewById(R.id.column2);
+        }
+    }
+}
