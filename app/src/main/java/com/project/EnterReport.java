a9e54d47f275c8850bb511786ec82b173ced2249
綾雪 <2239301685@qq.com>
2023-04-09 20:37:09 +0800

    EnterReport修改

---
+++ app/src/main/java/com/project/EnterReport.java
@@ -0,0 +1,154 @@
+package com.project;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.project.JDBC.ReportDao;
+import com.project.Pojo.Report;
+import com.project.Sqlite.UserLocalDao;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+// 体检报告录入
+public class EnterReport extends Fragment {
+
+    String organ;
+
+    String username;
+    String phone_number;
+
+    String date;
+    String hospital;
+    String type;
+
+    EditText editTextDate;
+    EditText editTextHospital;
+    EditText editTextType;
+
+    Bitmap bitmap;
+
+    public EnterReport(String organ) {
+        this.organ = organ;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.enter_report, container, false);
+
+        // Get username
+        try {
+            UserLocalDao userLocalDao = new UserLocalDao(getContext());
+            userLocalDao.open();
+            username = userLocalDao.getUser();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        // Set an OnClickListener on the button to launch the gallery
+        Button buttonUpload = view.findViewById(R.id.buttonUpload);
+        buttonUpload.setOnClickListener(v -> galleryLauncher.launch("image/*"));
+
+        // Get views
+        editTextDate = view.findViewById(R.id.editTextDate);
+        editTextHospital = view.findViewById(R.id.editTextHospital);
+        editTextType = view.findViewById(R.id.editTextType);
+
+        // Confirm button
+        Button buttonConfirm = view.findViewById(R.id.buttonConfirm);
+        buttonConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get data
+                date = editTextDate.getText().toString();
+                hospital = editTextHospital.getText().toString();
+                type = editTextType.getText().toString();
+
+                // Turn bitmap into string
+                String bitmapString = null;
+                if (bitmap != null) {
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                    byte[] byteArray = stream.toByteArray();
+                    bitmapString = Base64.encodeToString(byteArray, Base64.DEFAULT);
+                }
+
+                // Upload to database
+                Boolean insertStatus=false;
+                Log.i("主线程", "多线程数据库测试开始");
+                String finalBitmapString = bitmapString;
+                FutureTask<Boolean> futureTask = new FutureTask<>(() -> {                           //使用FutureTask创建可获得返回值的执行任务 泛型为返回值类型
+                    //Thread.sleep(2000);                                                           //使用sleep模拟耗时操作
+                    Boolean valueReturn=false;
+                    ReportDao reportDao = new ReportDao();
+                    Log.i("子线程", "准备调用数据库");
+                    reportDao.getConnection();                                                      //调用mysql记得连接
+                    Report report = new Report();
+                    reportDao.getConnection();
+                    report.setPhone_number(username);
+                    report.setReport_content(finalBitmapString);
+                    report.setReport_No(reportDao.reportCount(username));
+                    report.setReport_type(type);
+                    report.setReport_place(hospital);
+                    if(date.equals(""))                                                             //判定日期是否填写 未填写则设置为null
+                        report.setReport_date(null);
+                    else
+                        report.setReport_date(date);
+                    report.setIs_deleted("false");
+                    valueReturn=reportDao.insertReport(username, report);
+                    //ArrayList<Report> reportArrayList = reportDao.getReportList(username);
+                    reportDao.closeConnection();                                                    //关闭连接 我不确定不关闭会不会产生bug
+                    Log.i("子线程", "数据库调用结束,返回数据");
+                    return valueReturn;
+                });
+                new Thread(futureTask).start();                                                     //必须使用start,使用run会导致异常
+                try {
+                    insertStatus = futureTask.get();                                                //使用get方法获得返回值 需要异常处理
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Log.i("主线程", "报告插入情况" + insertStatus);
+                Log.i("主线程", "多线程数据库测试结束");
+
+                // Jump to organ page
+                FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, new Organ(organ));
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+        return view;
+    }
+
+    private final ActivityResultLauncher<String> galleryLauncher = registerForActivityResult(new ActivityResultContracts.GetContent(), uri -> {
+        bitmap = null;
+        try {
+            bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), uri);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    });
+}
---
+++ app/src/main/java/com/project/JDBC/ReportDao.java
@@ -0,0 +1,146 @@
+package com.project.JDBC;
+
+import com.project.Pojo.Report;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ReportDao extends JDBCHelper{
+    public ReportDao(){
+    }
+    public ArrayList<Report> getReportList(String account) {
+        ArrayList<Report> reportArrayList=new ArrayList<>();
+        String sql="SELECT report_No,report_content,report_type,report_place,report_date,is_deleted FROM report WHERE phone_number=? ORDER BY report_No";
+        try {
+            PreparedStatement preparedStatement=connection.prepareStatement(sql);
+            preparedStatement.setString(1,account);
+            ResultSet resultSet= preparedStatement.executeQuery();
+            while(resultSet.next()){
+                Report report_temp=new Report();
+                report_temp.setPhone_number(account);
+                report_temp.setReport_No(resultSet.getInt("report_No"));
+                report_temp.setReport_content(resultSet.getString("report_content"));
+                report_temp.setReport_type(resultSet.getString("report_type"));
+                report_temp.setReport_place(resultSet.getString("report_place"));
+                report_temp.setReport_date(resultSet.getString("report_date"));
+                report_temp.setIs_deleted(resultSet.getString("is_deleted"));
+                reportArrayList.add(report_temp);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return reportArrayList;
+    }
+    public Boolean updateReport(String account, Report report_update){
+        Boolean valueReturn=false;
+        String sql="UPDATE report SET report_content=?,report_type=?,report_place=?,report_date=?,is_deleted=? where phone_number=? and report_No=?";
+        try {
+            PreparedStatement preparedStatement=connection.prepareStatement(sql);
+            preparedStatement.setString(1,report_update.getReport_content());
+            preparedStatement.setString(2,report_update.getReport_type());
+            preparedStatement.setString(3,report_update.getReport_place());
+            preparedStatement.setString(4,report_update.getReport_date());
+            preparedStatement.setString(5,report_update.getIs_deleted());
+            preparedStatement.setString(6,account);
+            preparedStatement.setInt(7,report_update.getReport_No());
+
+            if(preparedStatement.executeUpdate()>0)
+            {
+                valueReturn=true;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return valueReturn;
+    }
+    public Boolean deleteReport(String account, Integer report_No){
+        Boolean valueReturn=false;
+        String sql="UPDATE report SET is_deleted='true' where phone_number=? and report_No=?";
+        if(report_No==null){
+            report_No=0;
+        }
+        try {
+            PreparedStatement preparedStatement=connection.prepareStatement(sql);
+            preparedStatement.setString(1,account);
+            preparedStatement.setInt(2,report_No);
+
+            if(preparedStatement.executeUpdate()>0){
+                valueReturn=true;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return valueReturn;
+    }
+    public  Boolean insertReport(String account, Report report_insert){
+        Boolean valueReturn=false;
+        String sql="INSERT INTO report (phone_number,report_content,report_No,report_type,report_place,report_date,is_deleted) VALUES (?,?,?,?,?,?,?)";
+        try {
+            PreparedStatement preparedStatement=connection.prepareStatement(sql);
+            preparedStatement.setString(1,account);
+            preparedStatement.setString(2,report_insert.getReport_content());
+            preparedStatement.setInt(3,report_insert.getReport_No());
+            preparedStatement.setString(4,report_insert.getReport_type());
+            preparedStatement.setString(5,report_insert.getReport_place());
+            preparedStatement.setString(6,report_insert.getReport_date());
+            preparedStatement.setString(7,"false");
+            if(preparedStatement.executeUpdate()>0){
+                valueReturn=true;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return valueReturn;
+    }
+    public void SyncReportUpload(String account,ArrayList<Report> reportArrayList) {
+        Stream<Report> alertStream=reportArrayList.stream();
+        alertStream.forEach(report -> {
+            if(is_Exist(account,report.getReport_No()))
+            {
+                updateReport(account,report);
+            }
+            else
+            {
+                insertReport(account,report);
+            }
+        });
+    }
+    private Boolean is_Exist(String account,Integer report_No) {
+        Boolean valueReturn=false;
+        String sql="SELECT phone_number FROM report WHERE phone_number=? AND report_No=?";
+        try {
+            PreparedStatement preparedStatement=connection.prepareStatement(sql);
+            preparedStatement.setString(1,account);
+            preparedStatement.setInt(2,report_No);
+            ResultSet resultSet=preparedStatement.executeQuery();
+            if(resultSet.next())
+            {
+                valueReturn=true;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return valueReturn;
+    }
+
+    public Integer reportCount(String account){
+        Integer valueReturn=0;
+        String sql="SELECT COUNT(report_No)as count FROM report WHERE phone_number=?";
+        try {
+            PreparedStatement preparedStatement=connection.prepareStatement(sql);
+            preparedStatement.setString(1,account);
+            ResultSet resultSet=preparedStatement.executeQuery();
+            if(resultSet.next())
+            {
+                valueReturn=resultSet.getInt("count")+1;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return valueReturn;
+    }
+}
