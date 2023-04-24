d046432f6906076853c066d373f07efd55731b15
windsnow1025 <windsnow1025@gmail.com>
2023-04-24 04:41:32 -0700

    Update WebView

--- webview/wwwroot/index.js
+++ webview/wwwroot/index.js
@@ -4,7 +4,7 @@ const http = require("http");
 const app = express();
 
 // HTTP
-const port = 3000;
+const port = 8080;
 http.createServer(app.listen(port, () => {
     console.log(`Server listening at port ${port}...`);
 }));
